82dd456f7c3e64d05e5896e8c0a4f7ccc1562144
davud47 <davudgraca47@gmail.com>
2026-09-24 23:20:02 +0200

    Service Worker: manuelles Update-Signal statt automatischem Übernehmen

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
-const CACHE_NAME = "stfd-v1";
+const CACHE_NAME = "stfd-v2";
 const CORE_ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("message", (event) => {
+  if (event.data === "SKIP_WAITING") self.skipWaiting();
+});
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => {})
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
